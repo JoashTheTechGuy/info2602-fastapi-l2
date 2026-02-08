1d5e56954055ae41b3e9dc2b10c078862d8c15c5
JoashTheTechGuy <bossmanpolite@gmail.com>
2026-02-08 16:44:16 -0400

    updated cli.py

--- app/models.py
+++ app/models.py
@@ -5,7 +5,19 @@ from pwdlib import PasswordHash
 password_hash = PasswordHash.recommended()
 
 class User(SQLModel, table=True):
-    id: Optional[int] =  Field(default=None, primary_key=True)
-    username:str = Field(index=True, unique=True)
-    email:str = Field(index=True, unique=True)
-    password:str
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(index=True, unique=True)
+    email: str = Field(index=True, unique=True)
+    password: str
+
+    def set_password(self, password: str) -> None:
+        self.password = password_hash.hash(password)
+
+    @classmethod
+    def create(cls, username: str, email: str, password: str):
+        user = cls(username=username, email=email, password="")
+        user.set_password(password)
+        return user
+
+    def __str__(self) -> str:
+        return f"(User id={self.id}, username={self.username}, email={self.email})"
